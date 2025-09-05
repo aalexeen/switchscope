5d45baa226aa3c15744bc6c8fd2a2ba10d0c5dda
aalexeen <alexan.sergeev@gmail.com>
2025-09-05 16:40:00 -0400

    Initial SpringBoot configuration.

---
+++ backend/src/test/java/io/switchscope/TestBackendApplication.java
@@ -0,0 +1,10 @@
+package io.switchscope;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(BackendApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+}
