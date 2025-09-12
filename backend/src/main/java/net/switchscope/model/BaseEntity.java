d78a55e05e87ea0a98c16d2b29c942453052c866
aalexeen <alexan.sergeev@gmail.com>
2025-09-12 10:37:41 -0400

    Integer ID changed to UUID. The fields createdAt and updatedAt added int BaseEntity and BaseTo. SWS-4. SWS-5

--- backend/src/main/java/net/switchscope/model/BaseEntity.java
+++ backend/src/main/java/net/switchscope/model/BaseEntity.java
@@ -4,7 +4,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import net.switchscope.HasId;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.UuidGenerator;
 import org.springframework.data.util.ProxyUtils;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
 
 @MappedSuperclass
 //  https://stackoverflow.com/a/6084701/548473
@@ -16,9 +22,20 @@ import org.springframework.data.util.ProxyUtils;
 public abstract class BaseEntity implements HasId {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @UuidGenerator
+    @Column(name = "id", updatable = false, nullable = false)
     @Schema(accessMode = Schema.AccessMode.READ_ONLY) // https://stackoverflow.com/a/28025008/548473
-    protected Integer id;
+    protected UUID id;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+    protected OffsetDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+    protected OffsetDateTime updatedAt;
 
     //    https://stackoverflow.com/questions/1638723
     @Override
@@ -35,7 +52,7 @@ public abstract class BaseEntity implements HasId {
 
     @Override
     public int hashCode() {
-        return id == null ? 0 : id;
+        return id == null ? 0 : id.hashCode();
     }
 
     @Override
--- backend/src/main/java/net/switchscope/to/BaseTo.java
+++ backend/src/main/java/net/switchscope/to/BaseTo.java
@@ -7,12 +7,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Data
 public abstract class BaseTo implements HasId {
     @Schema(accessMode = Schema.AccessMode.READ_ONLY) // https://stackoverflow.com/a/28025008/548473
-    protected Integer id;
+    protected UUID id;
+
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+    protected OffsetDateTime createdAt;
+
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+    protected OffsetDateTime updatedAt;
+
 
     @Override
     public String toString() {
