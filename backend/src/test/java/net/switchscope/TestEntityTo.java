0e3e271c0cb435b9eb19202a18ce7a46a73b6046
aalexeen <alexan.sergeev@gmail.com>
2026-01-15 10:51:47 -0500

    some test issue solved.

---
+++ backend/src/test/java/net/switchscope/TestEntityTo.java
@@ -0,0 +1,26 @@
+package net.switchscope;
+
+import net.switchscope.to.BaseTo;
+
+import java.util.UUID;
+
+public class TestEntityTo extends BaseTo {
+    private String name;
+
+    public TestEntityTo() {
+        super();
+    }
+
+    public TestEntityTo(UUID id, String name) {
+        super(id);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
